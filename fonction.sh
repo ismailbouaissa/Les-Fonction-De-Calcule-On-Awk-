e8640bba9319c1e9772bab77f231c9c3523f9785
ismailbouaissa <ismailbouaissa@gmail.com>
2018-05-05 23:07:03 +0100

    chi comment

--- fonction.sh
+++ fonction.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+awk '{print $0}' noteawk
+
+awk '{print $1}' noteawk
+
+awk ' {print $1,$3} ' noteawk
+
+awk "BEGIN { print \"Hello, world!!\" }"
+
+ 
+awk 'BEGIN { print "Here is a single \
+quote <'\''>" }'
+
+
+awk '{ if (length(__g5_token5ae1bf23db3bd) > max) max = \
+length(__g5_token5ae1bf23db3bd) }
+END { print max }' noteawk
+
+
