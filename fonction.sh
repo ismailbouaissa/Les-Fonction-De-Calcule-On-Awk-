dd55e8b2e3dad2336bd074cb23802b7d8a245b78
aitja <36170707+aitja@users.noreply.github.com>
2018-05-06 17:18:40 +0100

    Update fonction.sh

--- fonction.sh
+++ fonction.sh
@@ -16,4 +16,7 @@ awk '{ if (length(__g5_token5ae1bf23db3bd) > max) max = \
 length(__g5_token5ae1bf23db3bd) }
 END { print max }' noteawk
 
-
+awk '{print $1+$2+$3}' noteawk
+awk '{print $1-$3}' noteawk
+awk '{print $3*$2}' noteawk
+awk '{print $2/$1}' noteawk
